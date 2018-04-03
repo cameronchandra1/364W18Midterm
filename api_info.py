a5fcbf04ae8a9b7f0d463a5ec0f659db69249bca
Cameron Chandra <camchan@umich.edu>
2018-04-03 18:35:01 -0400

    Add files via upload

---
+++ api_info.py
@@ -0,0 +1,6 @@
+consumer_key = '2nVNkUHVLpPa2fwiqpJHL6Bqh'
+consumer_secret = 'QiOOXXrAXF84RNXw6uPJi9dwAp1gxznRl99wG4WZA00BmJkewX'
+access_token = "612102861-c65COsqLK8Dnpc1GNALEzuz311RI29ZEuXZPa2BA"
+access_token_secret = 'ycxAOxf2Xy7ZbR5NZkdtbpTQAzi0e6axGGku07NwgZrQF'
+movie_key= 'b209ecc8'
+nyt_key = '98eba02323d11b2615cb90216f95dc76:12:74970859'
