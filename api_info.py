56a45ba76f7aa478c503008cfa99e096ed3dab34
Cameron Chandra <camchan@umich.edu>
2018-04-03 18:34:40 -0400

    delete api_info.py

--- api_info.py
+++
@@ -1,4 +0,0 @@
-consumer_key = '2nVNkUHVLpPa2fwiqpJHL6Bqh'
-consumer_secret = 'QiOOXXrAXF84RNXw6uPJi9dwAp1gxznRl99wG4WZA00BmJkewX'
-access_token = "612102861-c65COsqLK8Dnpc1GNALEzuz311RI29ZEuXZPa2BA"
-access_token_secret = 'ycxAOxf2Xy7ZbR5NZkdtbpTQAzi0e6axGGku07NwgZrQF'
